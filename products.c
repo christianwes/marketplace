74c6b828c10947b29128fc6cd2e52c79723dc315
Christian Westrom <chernandezwestrom@frba.utn.edu.ar>
2024-05-27 21:05:21 -0300

    Feature/unit price (#2)
    * Add new feature to calculate
    
    * Add calculate_total_price function
    
    ---------
    
    Co-authored-by: Ignacio Crusoe <icrusoe@frba.utn.edu.ar>

--- products.c
+++ products.c
@@ -6,17 +6,28 @@ typedef struct {
     int quantity;
 } Product;
 
+float calculate_unit_price(float total_price, int quantity) {
+    return total_price / quantity;
+}
+
 void print_product(Product p) {
-    printf("Product: %s, Price: %.2f, Quantity: %d\n", p.name, p.price, p.quantity);
+    printf("Product: %s, Price: %.2f, Quantity: %d, Unit Price: %.2f\n", p.name, p.price, p.quantity, calculate_unit_price(p.price * p.quantity, p.quantity));
+}
+
+float calculate_total_price(Product p) {
+    return p.price / p.quantity;
 }
 
 int main() {
     Product p1 = {"Apple", 0.5, 30};
-    Product p = {"Banana", 0.2, 50};
+    Product p2 = {"Banana", 0.2, 50};
+    Product p3 = {"Orange", 0.3, 80};
+
 
     print_product(p1);
-    print_product(p);
+    print_product(p2);
+    print_product(p3);
+
 
     return 0;
 }
-
